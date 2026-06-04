6ecad4dc072f0b9405d552ae96c000bdddb742f3
David <davidvoler@gmail.com>
2026-06-04 16:29:28 +0300

    uploaded with annotation and ruby text

--- DDL/content/updates.sql
+++ DDL/content/updates.sql
@@ -7,3 +7,33 @@ create index to_lang_to_id on content_raw.translation_links (to_lang, to_id)
 
 
 create index lang_word1 on content_raw.sentence_elements (lang, word1)
+
+
+create table content_raw.secondary_words_translation (
+    id int8 NOT NULL,
+    lang varchar(12) NOT NULL,
+    word varchar(300) NOT NULL,
+    to_id int8 NOT NULL,
+    to_lang varchar(12) NOT NULL,
+    to_word varchar(300) NOT NULL,
+    to_to_id int8 NOT NULL,
+    to_to_lang varchar(12) NOT NULL,
+    to_to_word varchar(300) NOT NULL,
+    PRIMARY KEY (id, to_id, to_to_id)
+);
+
+
+
+
+insert into content_raw.secondary_words_translation
+select se.id as id, 'ja' as lang,  w.word as word, s.id as to_id, 'en' as to_lang, s.text as to_word , s1.id as to_to_id, 'he' as to_to_lang,  s1.text as to_to_word 
+from content_raw.words  w
+join content_raw.sentence_elements se 
+on w.word = se.text
+join content_raw.translation_links tl  on se.id = tl.id and tl.lang = 'ja' and tl.to_lang = 'en'
+join content_raw.sentences s on tl.to_id = s.id
+join content_raw.translation_links tl1 on tl1.id = s.id and tl1.to_lang ='he' 
+join content_raw.sentences s1 on tl1.to_id = s1.id 
+where w.lang = 'ja' and se.lang = 'ja'
+group by 1,2,3,4,5,6,7,8,9
+on conflict(id,to_id,to_to_id) do nothing
--- scripts/next_step_safe_jp_he.py
+++ scripts/next_step_safe_jp_he.py
@@ -13,7 +13,44 @@ from multiprocessing import Pool
 import os
 
 
+dictionary = {}
 
+
+def is_hiragana(char):
+    """Check if a character is Hiragana"""
+    return '\u3040' <= char <= '\u309F'
+
+def is_katakana(char):
+    """Check if a character is Katakana"""
+    return ('\u30A0' <= char <= '\u30FF') or ('\u31F0' <= char <= '\u31FF')
+
+def is_kanji(char):
+    """Check if a character is Kanji"""
+    return ('\u4E00' <= char <= '\u9FAF') or \
+           ('\u3400' <= char <= '\u4DBF') or \
+           ('\uF900' <= char <= '\uFAFF')
+
+
+def has_kanji_or_hiragana(word):
+    """Check if a word contains at least one Kanji or Hiragana character"""
+    for char in word:
+        if is_kanji(char) or is_hiragana(char):
+            return True
+    return False
+
+
+async def load_dictionary():
+    global dictionary
+    sql = f"""SELECT * FROM content_raw.secondary_words_translation"""
+    results = await get_query_results(sql, ())
+    for r in results:
+        word = r.get('word')
+        translation = r.get('to_to_word')
+        if has_kanji_or_hiragana(word):
+            dictionary[word] = translation
+            print (f"added word {word} to dictionary with translation {translation}")
+        else:
+            print(f"skipping word {word} as it does not contain kanji or hiragana")
 def add_words_for_recognize(word):
     if len(word) > 3:
         words_for_recognize.add(word)
@@ -73,10 +110,10 @@ def get_ruby(elements):
             romanji.append({'text': e.get('text'), 'ruby': e.get('roma', '')})
         else:
             romanji.append({'text': e.get('text', '')})
-    return hira, romanji, kana
+    return {"hiragana": hira, "katakana": kana, "romanji": romanji}
 
 async def gen_exercise(lang, to_lang, id, to_id, weight):
-    
+    global dictionary
     lang_sql = f"""
     SELECT text, elements, word1, word2, word3, words, options
     FROM content_raw.sentence_elements
@@ -103,6 +140,14 @@ async def gen_exercise(lang, to_lang, id, to_id, weight):
     options = r.get('options', [])
     to_text = r.get('to_text')
     to_options = r.get('to_options', [])
+    annotations  = []
+    if word1 and dictionary.get(word1):
+        annotations.append({"word": word1, "translation": dictionary.get(word1)})
+    if word2  and dictionary.get(word2):
+        annotations.append({"word": word2, "translation": dictionary.get(word2)})
+    if word3  and dictionary.get(word3):
+        annotations.append({"word": word3, "translation": dictionary.get(word3)})
+    
     words_for_recognize.add(word1)
     words_for_recognize.add(word2)
     words_for_recognize.add(word3)
@@ -113,6 +158,7 @@ async def gen_exercise(lang, to_lang, id, to_id, weight):
     to_options = to_options[:number_of_options]
     op = [{"text":o} for o in to_options]
     op.append({"text": to_text, "correct": True})
+    ruby_text = get_ruby(r.get('elements', []))
 
     random.shuffle(op)
     rnd= random.randint(0,10)
@@ -142,7 +188,8 @@ async def gen_exercise(lang, to_lang, id, to_id, weight):
                         "sentence_id": id,
                         "sentence_to_id": to_id,
                         "weight": weight,
-                        "elements": r.get('elements', [])
+                        "ruby_text": ruby_text,
+                        "annotations": annotations
                     }
                     return ex
     if rnd > 8 and len(words_for_recognize_split) > 150 and weight > 7:
@@ -164,7 +211,8 @@ async def gen_exercise(lang, to_lang, id, to_id, weight):
             'sentence_id': id,
             'sentence_to_id': to_id,
             'weight': weight,
-            'elements': r.get('elements', [])
+            'ruby_text': ruby_text,
+            'annotations': annotations
         }
         return ex
     else:
@@ -182,7 +230,8 @@ async def gen_exercise(lang, to_lang, id, to_id, weight):
         'word1': word1, 
         'word2': word2, 
         'word3': word3,
-        'elements': r.get('elements', [])
+        'ruby_text': ruby_text,
+        'annotations': annotations
     }
     return ex
 
@@ -231,12 +280,12 @@ async def gen_module(m:dict):
 async def gen_and_save_module(m:dict):
     print(f"generating module {m.get('module')}")
     module_no = int(m.get('module'))
-    os.makedirs(f"../data/content/ja_he/v1/module_{module_no}", exist_ok=True)
-    if len(os.listdir(f"../data/content/ja_he/v1/module_{module_no}")) > 0:
+    os.makedirs(f"../data/content/ja_he/v3/module_{module_no}", exist_ok=True)
+    if len(os.listdir(f"../data/content/ja_he/v3/module_{module_no}")) > 1:
         print(f"module {m.get('module')} already exists, skipping")
         return
     gen_m =  await gen_module(m)
-    with open(f"../data/content/ja_he/v1/module_{module_no}/module.yaml", 'w') as f:
+    with open(f"../data/content/ja_he/v3/module_{module_no}/module.yaml", 'w') as f:
         f.write(f"module: {module_no}\n")
         f.write(f"weight: {module_no}\n")
     i = 1
@@ -246,7 +295,7 @@ async def gen_and_save_module(m:dict):
         lesson_no = i
         weight = lesson.get('weight',lesson_no )
         title = lesson.get('title', '')
-        with open(f"../data/content/ja_he/v1/module_{module_no}/lesson_{lesson_no}.txt", 'w') as f:
+        with open(f"../data/content/ja_he/v3/module_{module_no}/lesson_{lesson_no}.txt", 'w') as f:
             title = lesson.get('lesson', '')
             f.write(f"title: {title}\n")
             f.write(f"weight: {weight}\n")
@@ -275,7 +324,8 @@ async def gen_and_save_module(m:dict):
                 if exercise.get('sentence_to_id'):
                     f.write(f"to_sentence_id: {exercise.get('sentence_to_id')}\n")
                 f.write(f"weight: {exercise.get('weight', i)}\n")
-                f.write(f"elements: {json.dumps(exercise.get('elements'))}\n")
+                f.write(f"ruby_text: {json.dumps(exercise.get('ruby_text'))}\n")
+                f.write(f"annotations: {json.dumps(exercise.get('annotations'))}\n")
                 for o in options:
                     prefix = "[-]"
                     if o.get('correct'):
@@ -287,6 +337,7 @@ def g_module(m:dict):
     asyncio.run(gen_and_save_module(m))
 
 async def gen_course(lang, to_lang, load_path):
+    await load_dictionary()
     module = yaml.safe_load(open(load_path, 'r'))
     modules = module.get('modules', [])
     for m in modules:
@@ -303,3 +354,4 @@ async def gen_course(lang, to_lang, load_path):
 if __name__ == '__main__':
     os.environ["POSTGRES_PORT"] = "5433"
     asyncio.run(gen_course('ja', 'he', '../data/content/ja_he/ja_he_course.yaml'))
+    
