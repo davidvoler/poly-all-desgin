1af9ac8cde98b0b825c580669f3d206a176a76c3
David <davidvoler@gmail.com>
2026-06-18 19:02:06 +0300

    different fields for exercise and other elements

---
+++ server/src/editor/corse_structure/fields.py
@@ -0,0 +1,34 @@
+EXERCISE_FIELDS = [
+'type', 
+'text', 
+'text_alt1', 
+'text_alt2', 
+'text_alt3',
+'voice', 
+'word1', 
+'word2', 
+'word3', 
+'sentence_id',
+'to_sentence_id', 
+'weight', 
+'ruby_text', 
+'annotations',
+'explanation',
+'correct_option',
+'incorrect_option',
+]
+LESSON_FIELDS = [
+'title',
+'weight',
+]
+MODULE_FIELDS = [
+'title',
+'weight',
+]
+COURSE_FIELDS = [
+'title',
+'lang',
+'to_lang',
+'description',
+'level',
+]
