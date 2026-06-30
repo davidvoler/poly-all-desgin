319808c887120b9f4c0821d04a1b3ce741314954
David <davidvoler@gmail.com>
2026-06-30 14:35:27 +0300

    removed unused fields

--- server/src/test_course_export.py
+++ server/src/test_course_export.py
@@ -4,4 +4,4 @@ from utils.course_export_from_db import export_course_from_db
 
 
 if __name__ == "__main__":
-    asyncio.run(export_course_from_db(38, "exported_course_from_db.yaml"))
+    asyncio.run(export_course_from_db(38, "../../data/content/exported_course_from_db.yaml"))
--- server/src/utils/course_export_from_db.py
+++ server/src/utils/course_export_from_db.py
@@ -3,42 +3,61 @@ import yaml
 from utils.db import get_query_results
 import asyncio
 
+EXERCISE_FIELDS_TO_REMOVE = ['course_id', 'exercise_id', 'lesson_id','module_id', 'created_at', 'updated_at']
+MODULE_FIELDS_TO_REMOVE = ['module_id', 'course_id', 'created_at', 'updated_at']
+LESSON_FIELDS_TO_REMOVE = ['lesson_id', 'module_id', 'created_at', 'updated_at']
+COURSE_FIELDS_TO_REMOVE = ['course_id', 'created_at', 'updated_at']
 
+def _remove_fields_and_nulls(data: dict, fields: list):
+    for field in fields:
+        if field in data:
+            del data[field]
+    for key in list(data.keys()):
+        if data[key] is None:
+            del data[key]
+    
 
 async def _export_exercise_data(exercise: dict, file):
+    _remove_fields_and_nulls(exercise, EXERCISE_FIELDS_TO_REMOVE)
     exercise_yaml = yaml.dump(exercise, default_flow_style=False, sort_keys=False, allow_unicode=True)
     file.write("type: exercise\n")
     file.write(exercise_yaml)
     file.write("---\n")
 
 async def _export_lesson_data(lesson: dict, file):
+    lesson_id = lesson.get('lesson_id')
+    _remove_fields_and_nulls(lesson, LESSON_FIELDS_TO_REMOVE)
     lesson_yaml = yaml.dump(lesson, default_flow_style=False, sort_keys=False, allow_unicode=True)
     file.write("type: lesson\n")
     file.write(lesson_yaml)
     file.write("---\n")
     sql = "SELECT * FROM course_simple.exercise WHERE lesson_id = %s"
-    exercises = await get_query_results(sql, (lesson.get('lesson_id'),))
+    exercises = await get_query_results(sql, (lesson_id,))
     for exercise in exercises:
         await _export_exercise_data(exercise, file)
 
 
 async def _export_module_data(module: dict, file):
+    module_id = module.get('module_id')
+    _remove_fields_and_nulls(module, MODULE_FIELDS_TO_REMOVE)
     module_yaml = yaml.dump(module, default_flow_style=False, sort_keys=False, allow_unicode=True) 
     file.write("type: module\n")
     file.write(module_yaml)
     file.write("---\n")
     sql = "SELECT * FROM course_simple.lesson WHERE module_id = %s"
-    lessons = await get_query_results(sql, (module.get('module_id'),))
+    lessons = await get_query_results(sql, (module_id,))
     for lesson in lessons:
         await _export_lesson_data(lesson, file)
 async def _export_course_data(course: dict, file):
+    course_id = course.get('course_id')
+    _remove_fields_and_nulls(course, COURSE_FIELDS_TO_REMOVE)
     course_yaml = yaml.dump(course, default_flow_style=False, sort_keys=False, allow_unicode=True)
     file.write("type: course\n")
     file.write(course_yaml)
     file.write("---\n")
     sql = "SELECT * FROM course_simple.module WHERE course_id = %s"
-    modules = await get_query_results(sql, (course.get('course_id'),))
-    print(f"Found {len(modules)} modules for course ID {course.get('course_id')}")
+    modules = await get_query_results(sql, (course_id,))
+    print(f"Found {len(modules)} modules for course ID {course_id}")
     for module in modules:
         print(f"Exporting module: {module}")
         await _export_module_data(module, file)
